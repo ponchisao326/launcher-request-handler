5c32d7dc3a988f8385d55ce24b16149d75bdbcde
ponchisao326_ <ponchisao326@hotmail.com>
2025-09-20 02:19:15 +0200

    Implement download_zip function in Downloader for downloading ZIP files

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,14 @@ pub struct JsonGrabber;
 pub struct Version {
     pub version: String,
 }
+
+impl Downloader {
+    /// Download a ZIP file from a given URL and save it to the specified destination.
+    pub fn download_zip(url: &str, destino: &str) -> Result<(), Box<dyn std::error::Error>> {
+        let response = reqwest::blocking::get(url)?;
+        let mut file = std::fs::File::create(destino)?;
+        let content = response.bytes()?;
+        std::io::copy(&mut content.as_ref(), &mut file)?;
+        Ok(())
+    }
+}
