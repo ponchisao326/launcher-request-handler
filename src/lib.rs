07be23b5a964c6e139808b269d5c81ac10de818c
ponchisao326_ <ponchisao326@hotmail.com>
2025-09-20 02:55:51 +0200

    Add update checking and details fetching to Update struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,41 @@
 use std::fs::File;
 use std::io::copy;
 use reqwest::blocking;
+use serde::de::DeserializeOwned;
 use serde::Deserialize;
 
 pub struct Downloader;
 pub struct JsonGrabber;
+pub struct Update;
 
 #[derive(Deserialize, Debug)]
 pub struct Version {
     pub version: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct UpdatePanelDetails {
+    pub new_version: String,
+    pub title: String,
+    pub description: String,
+    pub image_url: String,
+    pub date: String,
+    pub change_log: Vec<String>,
+    pub download_url: String
+}
+
+impl Update {
+    /// Check if a new version is available by comparing the current version with the latest version from a URL.
+    pub fn check_for_update(current_version: &str, url: &str) -> Result<bool, Box<dyn std::error::Error>> {
+        let latest_version: Version = JsonGrabber::verify_json(url)?;
+        Ok(latest_version.version != current_version)
+    }
+
+    /// Fetch update panel details from a given URL.
+    pub fn update_panel_details(url: &str) -> Result<UpdatePanelDetails, Box<dyn std::error::Error>> {
+        let details: UpdatePanelDetails = JsonGrabber::verify_json(url)?;
+        Ok(details)
+    }
 }
 
 impl Downloader {
@@ -24,9 +51,9 @@ impl Downloader {
 
 impl JsonGrabber {
     /// Download a JSON file from a given URL and parse it into a Response struct.
-    pub fn verify_json(url: &str) -> Result<Version, Box<dyn std::error::Error>> {
+    pub fn verify_json<T: DeserializeOwned>(url: &str) -> Result<T, Box<dyn std::error::Error>> {
         let response = blocking::get(url)?;
-        let json: Version = response.json()?;
+        let json = response.json::<T>()?;
         Ok(json)
     }
 }
